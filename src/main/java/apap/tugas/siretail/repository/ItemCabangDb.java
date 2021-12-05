fe0f62e57e6c09b8d91a12b3bcb08fbaf300a773
ichsanulamal <muhammad.ichsanul19@gmail.com>
2021-12-05 10:58:35 +0700

    refactor feat 13

--- src/main/java/apap/tugas/siretail/repository/ItemCabangDb.java
+++ src/main/java/apap/tugas/siretail/repository/ItemCabangDb.java
@@ -1,5 +1,6 @@
 package apap.tugas.siretail.repository;
 
+import apap.tugas.siretail.model.CabangModel;
 import apap.tugas.siretail.model.ItemCabangModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ItemCabangDb extends JpaRepository<ItemCabangModel, Integer> {
+    ItemCabangModel getItemCabangModelByItemIDAndAndCabang(String itemID, CabangModel cabangModel);
     ItemCabangModel getByItemID(String itemID);
     Optional<ItemCabangModel> findById(int id);
 }
