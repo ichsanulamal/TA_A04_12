9651e6dfee9217919c69c3dfe3905e1b31024eea
ichsanulamal <muhammad.ichsanul19@gmail.com>
2021-12-09 10:14:39 +0700

    refactor feat 13

--- src/main/java/apap/tugas/siretail/controller/ItemController.java
+++ src/main/java/apap/tugas/siretail/controller/ItemController.java
@@ -2,17 +2,12 @@ package apap.tugas.siretail.controller;
 
 import apap.tugas.siretail.model.CabangModel;
 import apap.tugas.siretail.model.ItemCabangModel;
-import apap.tugas.siretail.model.UserModel;
 import apap.tugas.siretail.repository.ItemCabangDb;
-import apap.tugas.siretail.model.UserModel;
 import apap.tugas.siretail.rest.CouponRestModel;
 import apap.tugas.siretail.rest.ListSiItemModel;
 import apap.tugas.siretail.rest.SiItemModel;
 import apap.tugas.siretail.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -20,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -161,9 +155,13 @@ public class ItemController {
             if (role.equals("Kepala Retail")) return true;
             else if (role.equals("Manager Cabang"))  {
                 CabangModel cabang = cabangService.getCabangByIdCabang(idCabang);
-                String username = ((UserDetails)principal).getUsername();
-                if (cabang.getPenanggungJawab().getUsername().equals(username)) {
-                    return true;
+                try {
+                    String username = ((UserDetails)principal).getUsername(); // error
+                    if (cabang.getPenanggungJawab().getUsername().equals(username)) {
+                        return true;
+                    }
+                } catch (Exception e) {
+                    return false;
                 }
             }
         }
