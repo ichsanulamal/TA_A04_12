f962c056a7311c939349e206a18824e48b9c1a58
ichsanulamal <75569370+ichsanulamal@users.noreply.github.com>
2021-12-09 10:35:24 +0700

    Refactor feature (#17)
    * test async
    
    * Revert "test async"
    
    This reverts commit ebe57e97
    
    * refactor feat 13
    
    * refactor feat 13
    
    * refactor
    
    * refactor

--- src/main/java/apap/tugas/siretail/controller/ItemController.java
+++ src/main/java/apap/tugas/siretail/controller/ItemController.java
@@ -3,15 +3,11 @@ package apap.tugas.siretail.controller;
 import apap.tugas.siretail.model.CabangModel;
 import apap.tugas.siretail.model.ItemCabangModel;
 import apap.tugas.siretail.repository.ItemCabangDb;
-import apap.tugas.siretail.model.UserModel;
 import apap.tugas.siretail.rest.CouponRestModel;
 import apap.tugas.siretail.rest.ListSiItemModel;
 import apap.tugas.siretail.rest.SiItemModel;
 import apap.tugas.siretail.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -19,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,7 +38,7 @@ public class ItemController {
 
     @Autowired
     private ItemCabangDb itemCabangDb;
-    
+
 //    @PreAuthorize("hasAuthority('Kepala Retail') or hasAuthority('Manager Cabang')")
     @GetMapping("/item/{idCabang}/add")
     public String addItemForm(Model model, @PathVariable Integer idCabang) {
@@ -160,9 +155,13 @@ public class ItemController {
             if (role.equals("Kepala Retail")) return true;
             else if (role.equals("Manager Cabang"))  {
                 CabangModel cabang = cabangService.getCabangByIdCabang(idCabang);
-                String username = ((UserDetails)principal).getUsername();
-                if (cabang.getPenanggungJawab().getUsername().equals(username)) {
-                    return true;
+                try {
+                    String username = ((UserDetails)principal).getUsername(); // error
+                    if (cabang.getPenanggungJawab().getUsername().equals(username)) {
+                        return true;
+                    }
+                } catch (Exception e) {
+                    return false;
                 }
             }
         }
