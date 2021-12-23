e8690ffe707ba099bb7f35d2cfbe85eea33028a4
dennyoctavian <dennyoctavian164@gmail.com>
2021-12-23 16:42:03 +0700

    coba debug lagi

--- src/main/java/apap/tugas/siretail/service/CabangServiceImpl.java
+++ src/main/java/apap/tugas/siretail/service/CabangServiceImpl.java
@@ -45,14 +45,15 @@ public class CabangServiceImpl implements CabangService {
     @Override
     public List<CabangModel> getAllCabangByManager(String username){
         List<CabangModel> dataCabang = cabangDb.findAll();
-        ArrayList<CabangModel> listCabang = new ArrayList<CabangModel>();
+        List<CabangModel> listCabang = new ArrayList<CabangModel>();
         for (CabangModel cabang : dataCabang) {
             String username_penanggung_jawab = cabang.getPenanggungJawab().getUsername();
             if (username_penanggung_jawab.equals(username)){
-                listCabang.add(cabang);
+                // listCabang.add(cabang);
+                System.out.println("udah masuk ke cek username");
             }
         }
-        return listCabang;
+        return dataCabang;
     }
 
     @Override
